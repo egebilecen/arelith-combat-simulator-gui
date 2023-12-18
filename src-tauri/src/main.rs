d0576f50561edd16d3142c67ad12ba0077dca1f3
Ege Bilecen <ege@r0m.cc>
2023-12-18 11:16:44 +0000

    builder reformatted to suppress unused warning in release build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,18 +5,21 @@ mod commands;
 use commands::*;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![is_debug])
-        .setup(|app| {
-            #[cfg(debug_assertions)]
-            {
-                use tauri::Manager;
-                
-                let window = app.get_window("main").unwrap();
-                window.open_devtools();
-            }
-            Ok(())
-        })
+    let tauri_builder =
+        tauri::Builder::default().invoke_handler(tauri::generate_handler![is_debug]);
+
+    #[cfg(debug_assertions)]
+    let tauri_builder = tauri_builder.setup(|app| {
+        {
+            use tauri::Manager;
+
+            let window = app.get_window("main").unwrap();
+            window.open_devtools();
+        }
+        Ok(())
+    });
+
+    tauri_builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
