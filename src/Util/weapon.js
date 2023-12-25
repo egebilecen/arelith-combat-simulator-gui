e2681516235f0500ac978b902cdc4687ce870d5d
Ege Bilecen <ege@r0m.cc>
2023-12-25 11:41:55 +0000

    added new util function to convert dice to string

--- src/Util/weapon.js
+++ src/Util/weapon.js
@@ -1,18 +1,26 @@
+export const getDiceStr = (dice, removeD1 = false) => {
+    let val = dice.rolls + "d" + dice.faces;
+
+    if (removeD1 && dice.faces === 1) {
+        return String(dice.rolls);
+    }
+
+    return val;
+};
+
 export const getWeaponBaseStr = (weaponBase, setName = true) => {
-    return (setName
-        ? weaponBase.name + " "
-        : "") +
-              "(" +
-              weaponBase.damage.rolls +
-              "d" +
-              weaponBase.damage.faces +
-              ", " +
-              (weaponBase.threat_range < 20
-                  ? weaponBase.threat_range + "-20"
-                  : "20") +
-              ", x" +
-              weaponBase.crit_multiplier +
-              ")";
+    return (
+        (setName ? weaponBase.name + " " : "") +
+        "(" +
+        getDiceStr(weaponBase.damage) +
+        ", " +
+        (weaponBase.threat_range < 20
+            ? weaponBase.threat_range + "-20"
+            : "20") +
+        ", x" +
+        weaponBase.crit_multiplier +
+        ")"
+    );
 };
 
 export const getWeaponStr = (weapon) => {
