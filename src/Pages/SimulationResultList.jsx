d7c7fb26b2e12c151b8c42dc28e16cf0ddc5c7f2
Ege Bilecen <ege@r0m.cc>
2023-12-28 21:02:45 +0000

    initial design for Results page for listing simulation results

---
+++ src/Pages/SimulationResultList.jsx
@@ -0,0 +1,92 @@
+import { invoke } from "@tauri-apps/api";
+import { useEffect, useState } from "react";
+import { Table } from "antd";
+import { Typography, Space, Divider, Popconfirm } from "antd";
+import PageContainer from "../Sections/PageContainer";
+import Loading from "../Components/Loading";
+
+const { Text } = Typography;
+
+function SimulationResultList() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [tableData, setTableData] = useState([]);
+
+    const textRenderer = (text) => <Text>{text}</Text>;
+    const dateRenderer = textRenderer;
+
+    const handleDeleteResult = (id) => {
+        console.log(id);
+    };
+
+    const cols = [
+        {
+            title: "ID",
+            dataIndex: "id",
+            render: textRenderer,
+        },
+        {
+            title: "Date",
+            dataIndex: ["obj", "timestamp"],
+            render: dateRenderer,
+        },
+        {
+            title: "Action",
+            dataIndex: "action",
+            render: (_, record) => (
+                <Space split={<Divider type="vertical" />}>
+                    <Popconfirm
+                        title="Warning"
+                        description="Are you sure to delete this record?"
+                        onConfirm={() => handleDeleteResult(record.id)}
+                        okText="Yes"
+                        cancelText="No"
+                        placement="left"
+                    >
+                        <a>delete</a>
+                    </Popconfirm>
+                </Space>
+            ),
+        },
+    ];
+
+    // Load stuff
+    useEffect(() => {
+        async function func() {
+            const [results] = await Promise.all([
+                invoke("get_rows", {
+                    table: "simulation_results",
+                }),
+            ]);
+
+            setIsLoading(false);
+
+            if (!results.success) {
+                // TODO
+            }
+
+            setTableData(
+                results.result.map((e, i) => {
+                    const temp = e;
+                    e.key = i;
+                    temp.obj = JSON.parse(e.json);
+
+                    return temp;
+                })
+            );
+        }
+
+        func();
+    }, []);
+
+    console.log(tableData);
+
+    return isLoading ? (
+        <PageContainer>
+            <Loading loading={isLoading} />
+        </PageContainer>
+    ) : (
+        <Table columns={cols} dataSource={tableData} />
+    );
+}
+
+export default SimulationResultList;
