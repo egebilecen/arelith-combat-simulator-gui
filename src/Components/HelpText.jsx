8e2dd56475fe3300cdccb3bf70aea52c1e2c40c6
Ege Bilecen <ege@r0m.cc>
2023-12-29 13:24:24 +0000

    added header and footer attributes to <HelpText>

--- src/Components/HelpText.jsx
+++ src/Components/HelpText.jsx
@@ -2,7 +2,15 @@ import { Tooltip, Row, Col, Typography } from "antd";
 import { isValidElement } from "react";
 const { Text } = Typography;
 
-function HelpText({ items, emptyText, listSymbol, placement, ...props }) {
+function HelpText({
+    header,
+    items,
+    footer,
+    emptyText,
+    listSymbol,
+    placement,
+    ...props
+}) {
     items ??= [];
     listSymbol ??= <>&#x2022; </>;
     placement ??= "bottom";
@@ -10,27 +18,54 @@ function HelpText({ items, emptyText, listSymbol, placement, ...props }) {
     return (
         <Tooltip
             title={
-                <Row>
-                    {items.length > 0 ? (
-                        items.map((e, i) =>
-                            isValidElement(e) ? (
-                                <Col key={"item-" + i} span={24}>{e}</Col>
-                            ) : (
-                                <Col key={"item-" + i} span={24}>
-                                    <Text
-                                        style={{
-                                            color: "inherit",
-                                        }}
-                                    >
-                                        {listSymbol}{e}
-                                    </Text>
-                                </Col>
-                            )
+                <>
+                    {header !== undefined ? (
+                        isValidElement(header) ? (
+                            header
+                        ) : (
+                            <Text style={{ color: "inherit" }} strong>
+                                {header}
+                            </Text>
                         )
                     ) : (
-                        <Col span={24}>{emptyText}</Col>
+                        <></>
                     )}
-                </Row>
+                    <Row>
+                        {items.length > 0 ? (
+                            items.map((e, i) =>
+                                isValidElement(e) ? (
+                                    <Col key={"item-" + i} span={24}>
+                                        {e}
+                                    </Col>
+                                ) : (
+                                    <Col key={"item-" + i} span={24}>
+                                        <Text
+                                            style={{
+                                                color: "inherit",
+                                            }}
+                                        >
+                                            {listSymbol}
+                                            {e}
+                                        </Text>
+                                    </Col>
+                                )
+                            )
+                        ) : (
+                            <Col span={24}>{emptyText}</Col>
+                        )}
+                    </Row>
+                    {footer !== undefined ? (
+                        isValidElement(footer) ? (
+                            footer
+                        ) : (
+                            <Text style={{ color: "inherit" }} strong>
+                                {footer}
+                            </Text>
+                        )
+                    ) : (
+                        <></>
+                    )}
+                </>
             }
             placement={placement}
         >
