16f132e0d2565a8aa0f4f300085d00696db00a39
Ege Bilecen <ege@r0m.cc>
2023-12-30 11:06:24 +0000

    added ECharts and implemented it

--- src/sub-src/ResultView/App.jsx
+++ src/sub-src/ResultView/App.jsx
@@ -1,38 +1,35 @@
 import { listen } from "@tauri-apps/api/event";
 import { useState, useEffect } from "react";
 import { Flex } from "antd";
+import ReactECharts from "echarts-for-react";
 import Loading from "../../Components/Loading";
 
 function App() {
     const [initialData, setInitialData] = useState(null);
+    const [graphData, setGraphData] = useState([]);
 
     const prepareGraphData = (data) => {
-        let graphData = [];
-        const simResults = data.map((simulation) => {
-            const avgResults = Object.keys(simulation.result.statistics).map(
-                (ac) => {
-                    const statistics = simulation.result.statistics[ac];
-                    const totalDamage = Object.keys(statistics.dmg_dealt)
-                        .map((dmgType) => statistics.dmg_dealt[dmgType])
-                        .reduce((a, b) => a + b, 0);
-                    const avgDmg = totalDamage / simulation.result.total_rounds;
+        const graphData = data.map((simulation) => {
+            const characterData = {
+                label: simulation.character.name,
+                xAxis: [],
+                yAxis: [],
+            };
 
-                    return {
-                        ac: parseInt(ac),
-                        avg_dmg: avgDmg,
-                        character: simulation.character.name,
-                    };
-                }
-            );
+            Object.keys(simulation.result.statistics).map((ac) => {
+                const statistics = simulation.result.statistics[ac];
+                const totalDamage = Object.keys(statistics.dmg_dealt)
+                    .map((dmgType) => statistics.dmg_dealt[dmgType])
+                    .reduce((a, b) => a + b, 0);
+                const avgDmg = totalDamage / simulation.result.total_rounds;
 
-            return avgResults;
+                characterData.xAxis.push(parseInt(ac));
+                characterData.yAxis.push(avgDmg);
+            });
+
+            return characterData;
         });
 
-        simResults.map((e) => {
-            graphData = graphData.concat(e);
-        });
-
-        console.log(graphData);
         return graphData;
     };
 
@@ -45,6 +42,11 @@ function App() {
 
         func();
     }, []);
+
+    useEffect(() => {
+        if (initialData !== null && initialData.data !== undefined)
+            setGraphData(prepareGraphData(initialData.data));
+    }, [initialData]);
 
     return (
         <>
@@ -60,7 +62,144 @@ function App() {
                     <Loading loading={true} size="large" />
                 </Flex>
             ) : (
-                <>Look for a plot library.</>
+                (() => {
+                    return (
+                        <ReactECharts
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                paddingTop: 6,
+                            }}
+                            option={{
+                                title: {
+                                    text:
+                                        "Simulation of " +
+                                        (initialData
+                                            ? initialData.data[0].result
+                                                  .total_rounds
+                                            : "X") +
+                                        " Rounds",
+                                    left: "center",
+                                    top: 0,
+                                },
+                                tooltip: {
+                                    trigger: "axis",
+                                    axisPointer: {
+                                        type: "line",
+                                    },
+                                    formatter: (params) => {
+                                        const sortedParams = params.sort(
+                                            (a, b) => b.data - a.data
+                                        );
+
+                                        const tooltipHtml = `
+                                            <div style='margin: 0px 0 0;line-height:1;'>
+                                                <div style='margin: 0px 0 0;line-height:1;'>
+                                                    <div style='font-size:14px;color:#666;line-height:1;'>
+                                                        <b>Target AC: </b> ${sortedParams[0].axisValue}
+                                                    </div>
+                                                    {labels}
+
+                                                    <div style='clear:both'></div>
+                                                </div>
+
+                                                <div style='clear:both'></div>
+                                            </div>
+                                        `;
+
+                                        return tooltipHtml.replace(
+                                            "{labels}",
+                                            sortedParams
+                                                .map((e) => {
+                                                    return `
+                                            <div style='margin: 6px 0 0;line-height:1;'>
+                                                <div style='margin: 0px 0 0;line-height:1;'>
+                                                    <div style='margin: 0px 0 0;line-height:1;'>
+                                                        ${e.marker}
+                                                        <span style='font-size:14px;color:#666;font-weight:400;'>${e.seriesName}</span>
+                                                        <span style='float:right;margin-left:20px;font-size:14px;color:#666;font-weight:900'>${e.value}</span>
+                                                        <div style='clear:both'></div>
+                                                    </div>
+
+                                                    <div style='clear:both'></div>
+                                                </div>
+                                            </div>
+                                        `;
+                                                })
+                                                .join("")
+                                        );
+                                    },
+                                },
+                                legend: {
+                                    data: graphData.map((e) => e.label),
+                                    orient: "horizontal",
+                                    left: "center",
+                                    top: 25,
+                                },
+                                grid: {
+                                    left: "4%",
+                                    right: "4%",
+                                    bottom: "3%",
+                                    top: 85,
+                                    containLabel: true,
+                                },
+                                toolbox: {
+                                    showTitle: false,
+                                    feature: {
+                                        saveAsImage: {
+                                            name: "result",
+                                            pixelRatio: 4,
+                                        },
+                                    },
+                                    top: 0,
+                                    right: 16,
+                                },
+                                xAxis: {
+                                    name: "Target AC",
+                                    nameLocation: "middle",
+                                    nameTextStyle: {
+                                        lineHeight: 24,
+                                    },
+                                    type: "category",
+                                    boundaryGap: false,
+                                    data:
+                                        graphData.length > 0
+                                            ? graphData[0].xAxis
+                                            : [],
+                                    splitLine: {
+                                        show: true,
+                                    },
+                                },
+                                yAxis: {
+                                    name: "Average Damage",
+                                    nameLocation: "middle",
+                                    nameTextStyle: {
+                                        lineHeight: 48,
+                                    },
+                                    type: "value",
+                                    min: 0,
+                                    splitLine: {
+                                        show: true,
+                                    },
+                                },
+                                series: graphData.map((e) => {
+                                    return {
+                                        name: e.label,
+                                        data: e.yAxis,
+                                        type: "line",
+                                        symbol: "circle",
+                                        symbolSize: 8,
+                                        lineStyle: {
+                                            width: 3,
+                                        },
+                                        // cursor: "default",
+                                    };
+                                }),
+                                animationDuration: 500,
+                            }}
+                        />
+                    );
+                })()
             )}
         </>
     );
