298041ed074f81e0d6dd64d9ecb507291152619a
Ege Bilecen <ege@r0m.cc>
2023-12-30 20:55:18 +0000

    added buy me a coffee button to about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,7 @@
-import { Typography } from "antd";
+import { Typography, Flex } from "antd";
 import PageContainer from "../Sections/PageContainer";
 
-const { Text } = Typography;
+const { Text, Link } = Typography;
 
 function AboutPage() {
     return (
@@ -18,20 +18,30 @@ function AboutPage() {
             />
             <Text style={{ display: "block", textAlign: "justify" }}>
                 This application is developed by{" "}
-                <a href="https://github.com/egebilecen" target="_blank">
+                <Link href="https://github.com/egebilecen" target="_blank">
                     Zaphiel
-                </a>
+                </Link>
                 . It simulates each round in combat, then summarizes the
                 statistics. It is open-source software, which can be found{" "}
-                <a
+                <Link
                     href="https://github.com/egebilecen/nwn-damage-calculator/tree/ui"
                     target="_blank"
                 >
                     here
-                </a>
+                </Link>
                 . If you like this application or find it useful, please
                 consider leaving a star ⭐ to the repo.
             </Text>
+            <Flex style={{ width: "100%", marginTop: 6 }} justify="center">
+                <Link
+                    href="https://www.buymeacoffee.com/egebilecen"
+                    target="_blank"
+                >
+                    <img src="buymeacoffee.png" style={{
+                        width: 250
+                    }} />
+                </Link>
+            </Flex>
         </PageContainer>
     );
 }
